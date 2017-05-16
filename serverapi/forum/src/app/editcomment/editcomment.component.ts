77f41dfd58bac6d7a5701e7f2a758fa1a4a51d7c
Marco Hanisch <marco.hanisch@tu-ilmenau.de>
2017-05-16 16:44:31 +0200

    nachtrag

---
+++ serverapi/forum/src/app/editcomment/editcomment.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { PostsService, User } from '../posts.service';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import 'rxjs/add/operator/switchMap';
+import { Location } from '@angular/common';
+
+@Component({
+  selector: 'app-editcomment',
+  templateUrl: './editcomment.component.html',
+  styleUrls: ['./editcomment.component.css']
+})
+export class EditcommentComponent implements OnInit {
+  comment: any =[];
+  constructor(private postsService: PostsService, private route: ActivatedRoute, private location: Location) { }
+
+  ngOnInit() {
+      this.route.params
+      .switchMap((params: Params) => this.postsService.getComment(params['comment_id'])).subscribe(comment => {
+        this.comment = comment
+      })
+  }
+  editComment(title: string, content: string, comment_id: string): void { 
+    this.postsService.editComment(title, content,comment_id).subscribe(comment =>this.comment.push(comment))
+    this.location.back();
+  }
+
+}
